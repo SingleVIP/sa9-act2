37a0480decc0a4f915d954bf0250181dc4a16361
peter <niqixian@gmail.com>
2024-02-23 12:32:48 -0600

    Completed

--- spec/prob5_spec.rb
+++ spec/prob5_spec.rb
@@ -2,21 +2,25 @@ require 'prob5'
 RSpec.describe TemperatureConverter do
   describe ".celsius_to_fahrenheit" do
     it "correctly converts 0°C to 32°F" do
-      # Fill in the test
+      result = TemperatureConverter.celsius_to_fahrenheit(0)
+      expect(result).to eq(32)
     end
 
     it "correctly converts 100°C to 212°F" do
-      # Fill in the test
+      result = TemperatureConverter.celsius_to_fahrenheit(100)
+      expect(result).to eq(212)
     end
   end
 
   describe ".fahrenheit_to_celsius" do
     it "correctly converts 32°F to 0°C" do
-      # Fill in the test
+      result = TemperatureConverter.fahrenheit_to_celsius(32)
+      expect(result).to eq(0)
     end
 
     it "correctly converts 212°F to 100°C" do
-      # Fill in the test
+      result = TemperatureConverter.fahrenheit_to_celsius(212)
+      expect(result).to eq(100)
     end
   end
 end
